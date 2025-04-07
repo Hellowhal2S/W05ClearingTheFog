32be9d7e29dae92457870abd9ee4ebe15734191e
wannaBpark <leehun456@hanmail.net>
2025-04-07 13:43:03 +0900

    Chores - 해상도 변경

--- Week0v5/Engine/Source/Runtime/Launch/LaunchEngineLoop.cpp
+++ Week0v5/Engine/Source/Runtime/Launch/LaunchEngineLoop.cpp
@@ -1,4 +1,4 @@
-﻿#include "LaunchEngineLoop.h"
+#include "LaunchEngineLoop.h"
 
 #include "EditorEngine.h"
 #include "Engine/Engine.h"
@@ -180,7 +180,7 @@ void FEngineLoop::WindowInit(HINSTANCE hInstance)
 
     hWnd = CreateWindowExW(
         0, WindowClass, Title, WS_POPUP | WS_VISIBLE | WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 1000, 1000,
+        CW_USEDEFAULT, CW_USEDEFAULT, 1300, 1000,
         nullptr, nullptr, hInstance, nullptr
     );
 }
